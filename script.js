22ee57256099710b2e0105cf2ea0025dbe8d25d9
Wendel Roberta <wendel.roberta1995@gmail.com>
2020-10-11 19:43:09 -0300

    Adicionando direções no jogo e impedindo que a cobrinha suma

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("cobrinha");
 let context = canvas.getContext("2d");
 let box = 32;
-let cobrinha = [];
+let cobrinha = []; //criar cobrinha como lista
 cobrinha[0] = {
     x: 8 * box,
     y: 8 * box
@@ -10,8 +10,8 @@ let direction = "right";
 
 
 function criarBG() {
-    context.fillStyle = "lightblue";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightpink";
+    context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y
 }
 
 function criarCobrinha() {
@@ -23,7 +23,22 @@ function criarCobrinha() {
 
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.ketCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+
 function iniciarJogo() {
+    if (cobrinha[0].x > 15 * box && direction == "right") cobrinha[0].x = 0;
+    if (cobrinha[0].x < 0 && direction == 'left') cobrinha[0].x = 16 * box;
+    if (cobrinha[0].y > 15 * box && direction == "down") cobrinha[0].y = 0;
+    if (cobrinha[0].y < 0 && direction == 'up') cobrinha[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -41,6 +56,7 @@ function iniciarJogo() {
         x: cobrinhaX,
         y: cobrinhaY
     }
+
     cobrinha.unshift(newHead);
 
 }
