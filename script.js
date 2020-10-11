1ff4d4217d0eb0f9658008c6cd0bcf21bde2fce3
Wendel Roberta <wendel.roberta1995@gmail.com>
2020-10-11 17:55:29 -0300

    criando a função cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let canvas = document.getElementById("cobrinha");
 let context = canvas.getContext("2d");
 let box = 32;
+let cobrinha = [];
+cobrinha[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+    for (i = 0; i < cobrinha.length; i++) {
+        context.fillStyle = "purple";
+        context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
+
+    }
+
+}
+
 criarBG();
+criarCobrinha();
